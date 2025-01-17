bd851be79d600e6ed6b7b233e10692c12d519a52
Ray-Yao-0917 <ray910917@chtsecurity.com>
2025-01-17 09:43:39 +0000

    migration

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'available'
+    ];
+
+    public function store() {
+        return $this->belongsTo('App\Models\Store');
+    }
+
+    public function authors(){
+        return $this->belongsToMany('App\Models\Author');
+    }
 }
